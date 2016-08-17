f13ad120e856624392ab2a69ffb9e4ea756b6d29
Kai Hoa <kaihoang@gmail.com>
2016-08-17 20:45:19 +0200

    add todos en todos/id

--- server.js
+++ server.js
@@ -2,8 +2,39 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	description: 'Mom belle',
+	completed: false
+}, {
+	id: 2,
+	description: 'Dad belle',
+	completed: false
+}];
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
+});
+
+// GET /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoId = req.params.id;
+
+	todos.forEach(function (todo) {
+		console.log(todo);
+	});
+
+	for (var key in todos) {
+		if (todos[key].id == todoId) {
+			res.json(todos[key]);
+			console.log(todos[key]);
+		}
+	}
+
 });
 
 app.listen(PORT, function () {
